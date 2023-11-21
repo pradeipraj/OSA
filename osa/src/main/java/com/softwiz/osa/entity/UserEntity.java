1b015f13e41bde036c88669a0f61d5880f802f65
Pradeep Raj <129373542+pradeipraj@users.noreply.github.com>
2023-11-21 11:59:49 +0530

    OSA Application
    OSA Application for user microservices....

---
+++ osa/src/main/java/com/softwiz/osa/entity/UserEntity.java
@@ -0,0 +1,36 @@
+package com.softwiz.osa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(name = "Users_Table")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "mobileNumber")
+    private String mobileNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
---
+++ osa/src/test/java/com/softwiz/osa/OsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwiz.osa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
